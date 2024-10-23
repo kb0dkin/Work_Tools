1d680a1ce8bafe2e2dbdbcfc99efe4998e8418fb
kb0dkin <kevinbodkin2017@u.northwestern.edu>
2024-10-23 08:24:18 -0500

    more threshold code

--- BrainPatch_analysis/threshold_xings.py
+++ BrainPatch_analysis/threshold_xings.py
@@ -19,7 +19,9 @@ def threshold_extract(sig, fs:float = 30000, b_high:float = 300, b_low:float = 6
                       isi_min:float = .75, simul_max:int = 10, channels:int|list = -1):
     '''
     threshold_extract
-        filters, pulls out thresholds, and puts the threshold data into a separate numpy array
+        1. filters
+        2. pulls out thresholds
+        3. puts the threshold data into a separate numpy array
 
     inputs:
         sig : np.array      - input signal, TxN
@@ -27,7 +29,7 @@ def threshold_extract(sig, fs:float = 30000, b_high:float = 300, b_low:float = 6
         b_high : float      - HPF frequency in hz [300]
         b_low : float       - LPF frequency in hz [6000]
         thresh : float      - standard deviation threshold multiplier [-3]
-        isi_min : float     - ISI time (in usec) to throw out spikes [.75]
+        isi_min : float     - ISI time (in ms) to throw out spikes [.75]
         simul_max : int     - max simultaneous channels within 1ms window (throwout crosstalk) [10]
         channels : int|list - channels to record from; -1 == all [-1]
        
@@ -44,6 +46,30 @@ def threshold_extract(sig, fs:float = 30000, b_high:float = 300, b_low:float = 6
     # filter
     filt_sos = signal.butter(N = 4, Wn = [b_high, b_low], btype = 'bandpass', fs = fs, output='sos')
     sig_filt = signal.sosfiltfilt(sos = filt_sos, x = sig, axis = 0)
+
+    # thresholding
+    thresholds = np.std(sig_filt, axis=0) * thresh # find the threshold value for each channel
+    xings = np.where(np.diff(int(sig_filt < thresholds)) > 1)
+
+    # remove unlikely units 
+    #   too many simultaneous channels in less than a specific amount of time
+    simul_starts = np.where(np.diff(xings[:,0], n=simul_max, axis=0) < (fs/1000))
+    simul_mask = np.ones(xings.shape[0],1) # initialize a mask
+    for ind in simul_starts: # remove all of the periods with too many multi-channel units
+        simul_mask[simul_starts:simul_starts+simul_max] = 0
+    xings = xings[simul_mask,:] # remove everything that happens too quickly
+
+    # too short ISI on a single channel.
+    valid_spikes = np.ndarray((0,2))
+    waveforms = np.ndarray((0,int(1.5 * (fs/1000))))
+    for chan in np.unique(xings[:,1]): # for each channel
+        chan_spikes = xings[xings[:,1] == chan, :]
+        chan_spikes = chan_spikes[[0, np.diff(chan_spikes) > isi_min * (fs/1000)+1],:]
+        valid_spikes = valid_spikes.append(chan_spikes, axis=0)
+        for spike in valid_spikes.iterrows():
+            
+
+    
 
 
 
